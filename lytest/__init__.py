9c2942510aac363fb66afc2603f9d93c078a84ce
Alex Tait <alexander.tait@nist.gov>
2018-10-09 17:18:48 -0600

    changed some stuff around. now its working for sure with klayout.db language

--- lytest/__init__.py
+++ lytest/__init__.py
@@ -4,13 +4,13 @@ try:
     kqp = ipc.generate_display_function(None, 'debugging.gds')
 except ImportError:
     def kqp(*args, **kwargs):
-        raise RuntimeError('klayout quickplot is not available.\nPlease install lyipc.')
+        print('klayout quickplot is not available.\nPlease install lyipc.')
 
 try:
     from phidl import quickplot2 as qp
 except ImportError:
     def qp(*args, **kwargs):
-        raise RuntimeError('phidl does not seem to be installed, so you cannot use qp')
+        print('phidl does not seem to be installed, so you cannot use qp')
 
 
 from lytest.kdb_xor import *

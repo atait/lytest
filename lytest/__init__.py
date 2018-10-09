a4c430f142fc4d3334b237d3c584950fc740e866
Alex Tait <alexander.tait@nist.gov>
2018-10-08 20:06:16 -0600

    made sure to use klayout.db

--- lytest/__init__.py
+++ lytest/__init__.py
@@ -1,8 +1,3 @@
-import lytest.kdb_xor
-import lytest.utest_buds
-import lytest.nonvisual
-import lytest.subcells
-
 from lytest.kdb_xor import *
 from lytest.utest_buds import *
 from lytest.nonvisual import *

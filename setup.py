61dacd1c95c90b6066da64d0d03df7632594a7c2
Alex Tait <alexander.tait@nist.gov>
2018-10-09 16:18:29 -0600

    removed autoinstall hooks. this is no longer recommended by lygadgets

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
 from setuptools import setup
 import os
 
-# Autoinstall into klayout
-try:
-    from lygadgets import postinstall_pure
-except (ImportError, ModuleNotFoundError):
-    print('\033[95mlygadgets not found, so klayout package not linked.')
-    print('Please download lygadgets from the klayout Package Manager\033[0m')
-    my_postinstall = dict()
-else:
-    setup_dir = os.path.dirname(os.path.realpath(__file__))
-    pkg_dir = os.path.join(setup_dir, 'lytest')
-    my_postinstall = {'install': postinstall_pure(pkg_dir)}
-
 
 def readme():
     with open('README.md', 'r') as fx:
@@ -23,11 +11,11 @@ setup(name='lytest',
       version='0.0.1',
       description='Regression testing for klayout and phidl',
       long_description=readme(),
-      author='Alex Tait',
+      author='Alex Tait, Adam McCaughan, Sonia Buckley, Jeff Chiles, Jeff Shainline, Rich Mirin, Sae Woo Nam',
       author_email='alexander.tait@nist.gov',
       license='MIT',
       packages=['lytest'],
-      install_requires=['klayout', 'pytest'],
+      install_requires=['klayout', 'pytest', 'lygadgets'],
       entry_points={},
-      cmdclass=my_postinstall,
+      cmdclass={},
       )

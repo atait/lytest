f57f83c3788ba530026b7731b5df6cef2f529b20
Alex Tait <alexander.tait@nist.gov>
2018-11-14 22:34:35 -0700

    version bump 0.9

--- lytest/__init__.py
+++ lytest/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 
 # quickplotters so you can debug more easily
 try:
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='lytest',
-      version='0.0.8',
+      version='0.0.9',
       description='Regression testing for klayout and phidl',
       long_description=readme(),
       author='Alex Tait, Adam McCaughan, Sonia Buckley, Jeff Chiles, Jeff Shainline, Rich Mirin, Sae Woo Nam',

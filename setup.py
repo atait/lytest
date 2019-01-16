5b4b4eab2c741736381b0e67d82c9bba0914f709
Alex Tait <atait@princeton.edu>
2019-01-15 17:26:41 -0700

    version bumps

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def readme():
 
 
 setup(name='lytest',
-      version='0.0.13',
+      version='0.0.14',
       description='Regression testing for klayout and phidl',
       long_description=readme(),
       author='Alex Tait, Adam McCaughan, Sonia Buckley, Jeff Chiles, Jeff Shainline, Rich Mirin, Sae Woo Nam',
       author_email='alexander.tait@nist.gov',
       license='MIT',
       packages=['lytest'],
-      install_requires=['klayout', 'pytest', 'lygadgets>=0.1.7', 'lyipc>0.2.0'],
+      install_requires=['klayout', 'pytest', 'lygadgets>=0.1.14', 'lyipc>0.2.0'],
       entry_points={'console_scripts': ['lytest=lytest.command_line:cm_main']},
       cmdclass={},
       )

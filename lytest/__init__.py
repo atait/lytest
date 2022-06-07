a2d5f4f9894509ba8bbc0a3a013e057700d85e7a
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-06-07 10:21:49 -0700

    run black

--- lytest/__init__.py
+++ lytest/__init__.py
@@ -1,23 +1,30 @@
-__version__ = '0.0.20'
+__version__ = "0.0.20"
 
 # quickplotters so you can debug more easily
 try:
     import lyipc.client as ipc
-    kqp = ipc.generate_display_function(None, 'debugging.gds')
+
+    kqp = ipc.generate_display_function(None, "debugging.gds")
 except ImportError:
+
     def kqp(*args, **kwargs):
-        print('klayout quickplot is not available.\nPlease install lyipc.')
+        print("klayout quickplot is not available.\nPlease install lyipc.")
+
+
 try:
     from lyipc.client import load as ipc_load
 except ImportError:
+
     def ipc_load(*args, **kwargs):
         pass
+
 
 try:
     from phidl import quickplot2 as qp
 except ImportError:
+
     def qp(*args, **kwargs):
-        print('phidl does not seem to be installed, so you cannot use qp')
+        print("phidl does not seem to be installed, so you cannot use qp")
 
 
 from lytest.kdb_xor import GeometryDifference, run_xor, xor_polygons_phidl
--- lytest/containers.py
+++ lytest/containers.py
@@ -7,23 +7,24 @@ from lytest import kqp, ipc_load
 
 @contextmanager
 def phidl_context(device_name=None, out_file=None):
-    ''' Handles a conditional write to file or send over lyipc connection.
-        The context manager yields a new empty Device.
-        The context block then modifies that device by adding references to it. It does not need to return anything.
-        Back to the context manager, the Device is saved if out_file is not None, or it is sent over ipc
+    """Handles a conditional write to file or send over lyipc connection.
+    The context manager yields a new empty Device.
+    The context block then modifies that device by adding references to it. It does not need to return anything.
+    Back to the context manager, the Device is saved if out_file is not None, or it is sent over ipc
 
-        Example::
+    Example::
 
-            with phidl_context(out_file='my_box.gds') as D:
-                r = D << phidl.geometry.rectangle(size=(10, 10), layer=1)
-                r.movex(20)
+        with phidl_context(out_file='my_box.gds') as D:
+            r = D << phidl.geometry.rectangle(size=(10, 10), layer=1)
+            r.movex(20)
 
-        will write the device with a rectangle to a file called 'my_box.gds' and do nothing with lyipc.
-        By changing out_file to None, it will send an ipc load command instead of writing to a permanent file,
-        (Although ipc does write a file to be loaded by klayout, it's name or persistence is not guaranteed.)
-    '''
+    will write the device with a rectangle to a file called 'my_box.gds' and do nothing with lyipc.
+    By changing out_file to None, it will send an ipc load command instead of writing to a permanent file,
+    (Although ipc does write a file to be loaded by klayout, it's name or persistence is not guaranteed.)
+    """
     from phidl import Device
-    TOP = Device('TOP')
+
+    TOP = Device("TOP")
     yield TOP
     if out_file is None:
         kqp(TOP, fresh=True)
@@ -33,12 +34,13 @@ def phidl_context(device_name=None, out_file=None):
 
 @contextmanager
 def pya_context(cell_name=None, out_file=None):
-    ''' Handles a conditional write to file or send over lyipc connection.
-        See phidl_context above.
-    '''
+    """Handles a conditional write to file or send over lyipc connection.
+    See phidl_context above.
+    """
     from lygadgets import pya
+
     layout = pya.Layout()
-    TOP = layout.create_cell('TOP')
+    TOP = layout.create_cell("TOP")
     yield TOP
     if out_file is None:
         kqp(TOP, fresh=True)
@@ -47,32 +49,34 @@ def pya_context(cell_name=None, out_file=None):
 
 
 def contained_arbitrary(func, layout_context):
-    '''
-        Converts a function that takes a Device argument to one that takes a filename argument.
-        This is used to develop fixed geometry creation blocks and then save them as reference files.
-        Bad idea to try to use this in a library or call it from other functions.
+    """
+    Converts a function that takes a Device argument to one that takes a filename argument.
+    This is used to develop fixed geometry creation blocks and then save them as reference files.
+    Bad idea to try to use this in a library or call it from other functions.
 
-        func should take *only one* argument that is a Device, modify that Device, and return nothing.
+    func should take *only one* argument that is a Device, modify that Device, and return nothing.
 
-        It's sort of a decorator version of save_or_visualize.
-        When called with a None argument, it will use klayout_quickplot.
+    It's sort of a decorator version of save_or_visualize.
+    When called with a None argument, it will use klayout_quickplot.
 
-        Example::
+    Example::
 
-            @contained_geometry
-            def boxer(D):
-                r = D << phidl.geometry.rectangle(size=(10, 10), layer=1)
-                r.movex(20)
+        @contained_geometry
+        def boxer(D):
+            r = D << phidl.geometry.rectangle(size=(10, 10), layer=1)
+            r.movex(20)
 
-        Usage::
+    Usage::
 
-            boxer()  # displays in klayout over ipc
-            boxer('temp.gds')  # saves to file instead
-    '''
+        boxer()  # displays in klayout over ipc
+        boxer('temp.gds')  # saves to file instead
+    """
+
     @wraps(func)
     def geometry_container(out_file=None):
         with layout_context(out_file=out_file) as TOP:
             func(TOP)
+
     return geometry_container
 
 
@@ -88,4 +92,5 @@ def contained_script(func):
             ipc_load(produced_file)
         else:
             os.rename(produced_file, out_file)
+
     return script_container

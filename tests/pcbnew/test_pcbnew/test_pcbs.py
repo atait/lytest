8035ea931091de74c54a1c248030d07869f1f4e2
Alex Tait <atait@ieee.org>
2024-01-28 02:46:43 -0500

    Fix test file roots when pytesting this package

--- tests/pcbnew/test_pcbnew/test_pcbs.py
+++ tests/pcbnew/test_pcbnew/test_pcbs.py
@@ -1,4 +1,7 @@
+import os
+import lytest
 from lytest import contained_pcbnewBoard, difftest_it
+lytest.utest_buds.test_root = os.path.dirname(__file__)
 
 @contained_pcbnewBoard
 def simple_track(pcb):
